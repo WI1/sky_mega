de3b2ed3274841c04fa10e08d223eef0f1ac0559
harryster <harisuman.naik@gmail.com>
2016-02-21 21:28:41 +0100

    error with meta regions

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,11 +1,11 @@
 <div id="page" class="<?php print $classes; ?>">
     <section id="meta">
         <div class="container">
-            <?php if ($left_meta): ?>
-                <div id="left-meta"><?php print $left_meta; ?></div>
+            <?php if ($page['left_meta']): ?>
+                <div id="left-meta"><?php print render($page['left_meta']); ?></div>
             <?php endif; ?>
-            <?php if ($right_meta): ?>
-                <div id="right-meta"><?php print $right_meta; ?></div>
+            <?php if ($pahe['right_meta']): ?>
+                <div id="right-meta"><?php print render($page['right_meta']); ?></div>
             <?php endif; ?>
         </div>
     </section>
