98d5dd30a1175115486c0f28f2d30db71282bbd4
harryster <harisuman.naik@gmail.com>
2016-03-02 11:20:48 +0100

    Added rw link

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,14 +1,16 @@
 <div id="page" class="<?php print $classes; ?>">
-    <section id="meta">
-        <div class="container">
-            <?php if ($page['left_meta']): ?>
-                <div id="left-meta"><?php print render($page['left_meta']); ?></div>
-            <?php endif; ?>
-            <?php if ($page['right_meta']): ?>
-                <div id="right-meta"><?php print render($page['right_meta']); ?></div>
-            <?php endif; ?>
-        </div>
-    </section>
+    <div id="fixed-top>
+        <section id="meta">
+            <div class="container">
+                <?php if ($page['left_meta']): ?>
+                    <div id="left-meta"><?php print render($page['left_meta']); ?></div>
+                <?php endif; ?>
+                <?php if ($page['right_meta']): ?>
+                    <div id="right-meta"><?php print render($page['right_meta']); ?></div>
+                <?php endif; ?>
+            </div>
+        </section>
+    </div>
   <header id="header" class="clearfix" role="banner">
     <div class="container">
     <?php if ($site_logo || $site_name || $site_slogan): ?>
@@ -41,7 +43,7 @@
 
     <?php print render($page['header']); ?>
     <?php print render($page['top_menu']); ?>
-        <div id="fau-logo"></div>
+        <a href="https://www.rw.fau.de/"><div id="fau-logo"></div></a>
     </div><!-- /end  limited width container -->
   </header>
   <section id="hero" class="clearfix" role="banner">
